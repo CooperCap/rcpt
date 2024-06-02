2c3f860f9fc87fefc8a5e82249967bc02c09c6c7
CooperCap <mjawadrahmans@gmail.com>
2024-06-02 15:20:42 +0400

    Made file become accessed as read-only

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use curl::easy;
 use std::{io::Write,  fs::File};
 
 fn main() {
+
     let mut curling = easy::Easy::new();
-    let mut store_file = match File::open("config.temp") {
+    let mut store_file = match File::create("config.temp") {
         Ok(file) => file,
-        Err(Errormesg) => File::create("config.temp").unwrap()
+        Err(_Errormesg) => File::create("config.temp").unwrap()
     };
 
     store_file.write(b"Hello bro").unwrap();
     match curling.url("https://www.google.com") {
-        Ok(_) => print!("Success!\n"),
+        Ok(_) => print!("Accessing google a success!\n"),
         Err(_) => print!("Problem bro, \n")
     }
     match curling.perform() {
@@ -18,8 +19,6 @@ fn main() {
         Err(_) => println!("PWP")
     };
     let thing = chrono::Local::now();
-    //My first comment
     println!("Hello, world!, The time rn is {:?}", thing.time());
-
-    println!("Hello world");
+    return 0;
 }
